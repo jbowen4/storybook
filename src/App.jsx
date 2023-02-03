10129c69bd595a2f83f4ebfdb53cf58b8f83818f
Vladyslav Budichenko <boodyvo@gmail.com>
2023-02-03 02:58:30 -0500

    add some update for video generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,163 @@
 import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import  TextField from "@mui/material/TextField";
+import  FormControl from "@mui/material/FormControl";
+import  InputLabel from "@mui/material/InputLabel";
+import  Select from "@mui/material/Select";
+import  FormControlLabel from "@mui/material/FormControlLabel";
+import  Button from "@mui/material/Button";
+import Grid from '@mui/material/Unstable_Grid2';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 import Lottie from 'lottie-react';
 import wizardAnimation from './assets/wizard.json';
+import {Box} from "@mui/material";
 
-const baseURL = process.env.BASE_URL;
+// const baseURL = process.env.BASE_URL;
+const baseURL = "https://1hp4qvie0k.execute-api.us-east-1.amazonaws.com/production";
 let testing_img = false;
+
+function StandardImageList({images, handleChosen, chosen}) {
+  console.log('images', images)
+  let rowImages = []
+  for(let setIndex in images) {
+    if (!images.hasOwnProperty(setIndex)) {
+      continue
+    }
+
+    let imageSet = images[setIndex]
+    for(let index in imageSet) {
+      if (!imageSet.hasOwnProperty(index)) {
+        continue
+      }
+
+      rowImages.push({
+        img: imageSet[index],
+        index: index,
+        set: setIndex,
+      })
+    }
+  }
+
+  console.log(chosen)
+
+  return (
+      <ImageList cols={2} >
+        {rowImages ? rowImages.map((item) => {
+              return <ImageListItem key={item.set + item.index}>
+                <img
+                    // src={`${item}?w=164&h=164&fit=crop&auto=format`}
+                    src={`${item.img}?w=164&auto=format`}
+                    // srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                    srcSet={`${item.img}?w=164&auto=format&dpr=2 2x`}
+                    alt="some image"
+                    loading="lazy"
+                    style={{
+                      opacity: chosen && chosen[item.set] && chosen[item.set][item.index] === 1 ? 0.3 : 1
+                    }}
+                    onClick={(e) => {handleChosen(item.set, item.index)}}
+                />
+              </ImageListItem>
+        }
+        ): null}
+      </ImageList>
+  );
+}
 
 function App() {
   const [body, setBody] = useState('');
   const [loading, setLoading] = useState(false);
-  const [loading2, setLoading2] = useState(false);
-  const [scenes, setScenes] = useState([]);
-  const [images, setImages] = useState([]);
-  const [video, setVideo] = useState(null);
+  const [loadingImages, setLoadingImages] = useState(false);
+  const [scenes, setScenes] = useState([
+    "The 3-head creepy creature attacked the small American village with no mercy. Everyone was running for their lives, but nobody could escape {monster growl}. People were screaming for help, but no one could hear them {male scream}. Everyone was so scared, their hearts were beating like crazy {heartbeat}. In a matter of minutes, the creature ate everyone in the village {glass smash}.",
+  "The survivors were left in shock and disbelief. They were too afraid to go back, so they just stayed and watched from a distance as the creature was eating their loved ones {church bell}. It was a nightmare that seemed to last forever {squeak}.",
+  "The creature finally left the village, leaving behind a trail of destruction and despair {thunder}. The villagers will never forget the terror they experienced, and the dark night will always haunt their dreams {wind}.",
+
+
+]);
+  const [chosen, setChosen] = useState([
+      [1, 0],
+        [1, 0],
+        [1, 0],
+  ]);
+  // [[
+  //   'https://replicate.delivery/pbxt/JzbZopDl4EbHDBWrdhAxNBbj3tNcEg6fkeIUruIHku8bYgaQA/out-0.png',
+  //   'https://replicate.delivery/pbxt/JzbZopDl4EbHDBWrdhAxNBbj3tNcEg6fkeIUruIHku8bYgaQA/out-0.png'
+  // ],
+  // [
+  //   'https://replicate.delivery/pbxt/TTJlNSoUHIqyK9XXsg7AXIxCtrs8Jfsi80PR7IzzKEPRMQNIA/out-0.png',
+  //   'https://replicate.delivery/pbxt/TTJlNSoUHIqyK9XXsg7AXIxCtrs8Jfsi80PR7IzzKEPRMQNIA/out-0.png',
+  // ],
+  // [
+  //   'https://replicate.delivery/pbxt/KnNcj5S2kSYbM1RgA9Ux9PiZFXWUNxjqRSelz5mVZCTUMQNIA/out-0.png',
+  //   'https://replicate.delivery/pbxt/KnNcj5S2kSYbM1RgA9Ux9PiZFXWUNxjqRSelz5mVZCTUMQNIA/out-0.png',
+  // ]]
+  const [images, setImages] = useState();
+  const [video, setVideo] = useState();
+
+  console.log('chosen global', chosen)
+
+  async function handleGenerateVideo() {
+    let requestScenes = []
+    for (let i = 0; i < chosen.length; i++) {
+      let scene = {
+        scene: scenes[i],
+      }
+        for (let j = 0; j < chosen[i].length; j++) {
+            if (chosen[i][j] === 1) {
+              console.log(images)
+              console.log('picked image', images[i][j])
+              scene.image = images[i][j]
+
+              break
+            }
+        }
+
+      requestScenes.push(scene)
+    }
+
+    let body = {scenes: requestScenes}
+    console.log('body', body)
+
+    const res = await fetch(`${baseURL}/api/v1/video`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      method: 'POST',
+      body: JSON.stringify(body),
+    });
+
+    const videoResult = await res.json();
+    console.log('videoResult', videoResult)
+    setVideo(videoResult.video);
+  }
+
+  function handleChosen(set, index) {
+    console.log("handleChosen", set, index)
+    let rows = []
+    for(let i = 0; i < images.length; i++){
+      rows[i] = []
+      for(let j = 0; j < images[i].length; j++) {
+        if (i == set) {
+          rows[i].push(j == index ? 1 : 0)
+
+          continue
+        }
+
+        rows[i].push(chosen[i][j])
+      }
+    }
+    // let newChosen = chosen
+    // newChosen[set] = object.chosenRow
+
+    console.log('going to set chosen', rows)
+
+    setChosen(rows)
+  }
 
   const getVideo = async () => {
     //{”scenes”: [{”scene”: “text of scene”, “image”: “image for scene”}]}
@@ -40,32 +183,45 @@ function App() {
   };
 
   const getImages = async () => {
-    setImages([
-      'https://replicate.delivery/pbxt/JzbZopDl4EbHDBWrdhAxNBbj3tNcEg6fkeIUruIHku8bYgaQA/out-0.png',
-      'https://replicate.delivery/pbxt/TTJlNSoUHIqyK9XXsg7AXIxCtrs8Jfsi80PR7IzzKEPRMQNIA/out-0.png',
-      'https://replicate.delivery/pbxt/KnNcj5S2kSYbM1RgA9Ux9PiZFXWUNxjqRSelz5mVZCTUMQNIA/out-0.png',
-    ]);
+    setLoadingImages(true);
+    try {
+      let imageSets = []
+      for(let i = 0; i < 2; i++) {
+        const res = await fetch(`${baseURL}/api/v1/images`, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+          method: 'POST',
+          body: JSON.stringify({
+            scenes: scenes,
+          }),
+        });
+        const results = await res.json();
 
-    // setLoading2(true);
-    // const res = await fetch(`${baseURL}/api/v1/images`, {
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //     'Access-Control-Allow-Origin': '*',
-    //   },
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     scenes: scenes,
-    //   }),
-    // });
+        const resImages = results['images']
+        console.log(results);
+        for(let resIndex in resImages) {
+          if (!resImages.hasOwnProperty(resIndex)) {
+            continue
+          }
 
-    // const results = await res.json();
+          if (!imageSets[resIndex]) {
+            imageSets[resIndex] = []
+          }
+          imageSets[resIndex].push(...resImages[resIndex])
+        }
+      }
 
-    // console.log(results);
 
-    // setImages(results['images'].flat(1));
-    // console.log(images);
-    // setLoading2(false);
+      setImages(imageSets);
+      console.log(images);
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setLoadingImages(false);
+    }
   };
 
   const getStory = async () => {
@@ -93,43 +249,78 @@ function App() {
   };
 
   return (
-    <div className='App'>
-      <h1 class='header'>Storybook: The simplest way to make beautiful story videos</h1>
-      <div class='container'>
-        <div class='inner-box'>
-          <textarea
-            name='input'
-            cols='40'
-            rows='5'
-            placeholder='Write your story prompt here...'
-            value={body}
-            onChange={(e) => setBody(e.target.value)}></textarea>
-          <button onClick={() => getStory()}>Submit</button>
-        </div>
-        {loading && scenes.length == 0 ? (
-          <div class='inner-box'>
+    <Box className='App'>
+      <h1 className='header'>Storybook: The simplest way to make beautiful story videos</h1>
+      <Grid
+          container
+          spacing={{xs: 0, sm: 2, md: 2}}
+          alignItems="center"
+          justifyContent="center"
+      >
+        <Grid paddingX={2} container alignItems="center" justifyContent="center" direction="column" item
+              xs={12} sm={12} md={12}>
+          <TextField
+              sx={{marginY: 1, minWidth: 200}}
+              label="What the story is about?"
+              fullWidth
+              size="medium"
+              placeholder="Write your story prompt here..."
+              multiline
+              value={body}
+              onChange={(e) => setBody(e.target.value)}></TextField>
+          <Button
+              variant="contained"
+              sx={{backgroundColor: '#000', color: '#fff', marginY: 1, minWidth: 200}}
+              onClick={() => getStory()}>
+            Generate Script
+          </Button>
+        </Grid>
+      </Grid>
+      <Box className='container'>
+        {loading && scenes.length === 0 ? (
+          <div className='inner-box'>
             <Lottie animationData={wizardAnimation} loop={true} />
-            <p class='text'>Loading...</p>
+            <p className='text'>Loading...</p>
           </div>
         ) : (
-          <div class='inner-box'>
+          <div className='inner-box'>
             {scenes.map((scene, index) => (
-              <p class='scene' key={index}>
+              <p className='scene' key={index}>
                 {scene}
               </p>
             ))}
-            {scenes.length > 0 && <button onClick={() => getImages()}>Generate images</button>}
           </div>
         )}
         {}
-      </div>
-      {loading2 && <Lottie animationData={wizardAnimation} loop={true} />}
-      {images.map((img) => {
-        return <img src={img} alt='Image' width='300' height='400' />;
-      })}
-      <button onClick={() => getVideo()} />
-      {video && <video src={video} width='750' height='500' controls></video>}
-    </div>
+      </Box>
+
+      {scenes.length > 0 &&
+          <Button
+              variant="contained"
+              sx={{backgroundColor: '#000', color: '#fff', marginY: 1, minWidth: 200}}
+              onClick={() => getImages()}>
+            Generate images
+          </Button>
+      }
+
+      {loadingImages && <Lottie animationData={wizardAnimation} loop={true} />}
+      {images ?
+          <Box>
+            <StandardImageList handleChosen={handleChosen} chosen={chosen} images={images} />
+            <Button
+                variant="contained"
+                sx={{backgroundColor: '#000', color: '#fff', marginY: 1, minWidth: 200}}
+                onClick={() => handleGenerateVideo()}>
+              Generate Video
+            </Button>
+          </Box>
+          : null}
+
+
+
+      {/*{video ? <video src={video} width='750' height='500' controls></video> : null}*/}
+      {video ? <a target="_blank" href={video} >Click here in several minutes to see the video</a> : null}
+    </Box>
   );
 }
 
